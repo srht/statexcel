494910a2a73562fff2265edaaa88a6db0e5d4513
srht <srhtkrgl@gmail.com>
2021-05-07 02:39:36 +0300

    handlebars added

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const exphbs=require('express-handlebars');
 const formidable = require('formidable');
+const mathjs = require('mathjs');
 const reader = require('xlsx')
 const app = express();
+
+app.engine('hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs'
+}));
+
+app.set('view engine', 'hbs');
+
 app.get('/', function (req, res){
-    res.sendFile(__dirname + '/index.html');
+    res.render('uploadform');
 });
 
 app.post('/', function (req, res){
+    const errorData=[]
+    const statsData=[]
     const form = new formidable.IncomingForm();
     
     form.parse(req, (err, fields)=>{
@@ -24,12 +36,11 @@ app.post('/', function (req, res){
         // Reading our test file
         const readFile = reader.readFile(file.path)
         
-        let data = []
-        let errorData=[]
+        const data = []
         const sheets = readFile.SheetNames
         console.log(sheets[0])
         const temp = reader.utils.sheet_to_json(readFile.Sheets[readFile.SheetNames[0]], {defval:""})
-
+        // console.log(sheet['My Sheet Name']['B3'].v);
         let rowNumber=0
           temp.forEach((res) => {
             let rowArr=[]
@@ -41,9 +52,9 @@ app.post('/', function (req, res){
                 let trimmed=value.trim()
                 let parsedNumberVal=Number(trimmed)
                 if(!value || isNaN(parsedNumberVal)){
-                    const cell = reader.utils.encode_cell({c:colNum, r:rowNumber})
+                    const cellCode = reader.utils.encode_cell({c:colNum, r:rowNumber})
                     //console.log(`${key} satır ${parsedNumberVal} sütun hatalı`)
-                    errorRow.push({column:key, rowNumber, cell })
+                    errorRow.push({column:key, rowNumber, cellCode })
                     rowArr.push(0)
                 }
                 else{
@@ -62,8 +73,33 @@ app.post('/', function (req, res){
           })
         //  res.setHeader('Content-Type', 'application/json');
     //res.end(JSON.stringify(data));
-    for(let rowNum=0;rowNum<data.length;rowNum++)
-    console.log(`${data[rowNum][0]} ${data[rowNum][2]}`)
+    //for(let rowNum=0;rowNum<data.length;rowNum++)
+    //console.log(`${data[rowNum][0]} ${data[rowNum][2]}`)
+
+    const transpose = arr => arr.reduce((m, r) => (r.forEach((v, i) => (m[i] ??= [], m[i].push(v))), m), [])
+
+    /*
+    
+*/
+let transpozedData=transpose(data)
+
+for (let columnNumber = 0; columnNumber < transpozedData.length; columnNumber++) {
+    const cell = reader.utils.encode_cell({c:columnNumber, r:1})
+    console.log(`${cell[0]} sütunu:`)
+    
+    const min=mathjs.min(transpozedData[columnNumber])
+    console.log(`Min: ${min}`)
+    const max=mathjs.max(transpozedData[columnNumber])
+    console.log(`Max: ${max}`)
+    const mean=mathjs.mean(transpozedData[columnNumber])
+    console.log(`Mean: ${mean}`)
+    const median=mathjs.median(transpozedData[columnNumber])
+    console.log(`Median: ${median}`)
+    statsData.push({
+        cell:cell[0], min, max, mean, median
+    })
+}
+
         //console.log(reader.utils.encode_cell({c:2, r:4}))
         /*
         for(let i = 0; i < sheets.length; i++)
@@ -80,11 +116,11 @@ app.post('/', function (req, res){
         // Printing data
         //console.log(data[0]['Hedef'])
         //console.log(errorData)
+
+        res.render('uploadform', {errorData:errorData.slice, statsData});
     }
     });
-
     
-    res.sendFile(__dirname + '/index.html');
 });
  
 app.listen(3000, () => {
