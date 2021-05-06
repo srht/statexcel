53af8ac16215319be6ddc2f67d394ad7c825ab15
srht <srhtkrgl@gmail.com>
2021-05-06 23:59:49 +0300

    errored cells detection

--- app.js
+++ app.js
@@ -25,24 +25,65 @@ app.post('/', function (req, res){
         const readFile = reader.readFile(file.path)
         
         let data = []
-        
+        let errorData=[]
         const sheets = readFile.SheetNames
-        
+        console.log(sheets[0])
+        const temp = reader.utils.sheet_to_json(readFile.Sheets[readFile.SheetNames[0]], {defval:""})
+
+        let rowNumber=0
+          temp.forEach((res) => {
+            let rowArr=[]
+            let errorRow=[]
+            let colNum=0
+            for (var key in res){
+                let value = res[key].toString();
+                //console.log(key + ": " + value);
+                let trimmed=value.trim()
+                let parsedNumberVal=Number(trimmed)
+                if(!value || isNaN(parsedNumberVal)){
+                    const cell = reader.utils.encode_cell({c:colNum, r:rowNumber})
+                    //console.log(`${key} satır ${parsedNumberVal} sütun hatalı`)
+                    errorRow.push({column:key, rowNumber, cell })
+                    rowArr.push(0)
+                }
+                else{
+                    rowArr.push(parsedNumberVal)
+                }
+                //console.log(Number(trimmed))
+                colNum++
+              }
+              /*console.log(res['PREOP CA /POSTOP CA'])
+              let preop=res['PREOP CA /POSTOP CA'];
+              let numbers=preop.split('/').map((n)=>Number(n))
+              data.push({ name: res[3], numbers: numbers })*/
+              data.push(rowArr)
+              errorData.push(errorRow)
+              rowNumber++
+          })
+        //  res.setHeader('Content-Type', 'application/json');
+    //res.end(JSON.stringify(data));
+    for(let rowNum=0;rowNum<data.length;rowNum++)
+    console.log(`${data[rowNum][0]} ${data[rowNum][2]}`)
+        //console.log(reader.utils.encode_cell({c:2, r:4}))
+        /*
         for(let i = 0; i < sheets.length; i++)
         {
-        const temp = reader.utils.sheet_to_json(
-            readFile.Sheets[readFile.SheetNames[i]])
+            const temp = reader.utils.sheet_to_json(readFile.Sheets[readFile.SheetNames[i]])
             temp.forEach((res) => {
-                console.log(res['GMT Erişim Tarihi'])
-                data.push(res)
+                console.log(res['PREOP CA /POSTOP CA'])
+                /*let preop=res['PREOP CA /POSTOP CA'];
+                let numbers=preop.split('/').map((n)=>Number(n))
+                data.push({ name: res[3], numbers: numbers })
             })
         }
-        
+        */
         // Printing data
         //console.log(data[0]['Hedef'])
+        //console.log(errorData)
     }
     });
 
+    
     res.sendFile(__dirname + '/index.html');
 });
  
